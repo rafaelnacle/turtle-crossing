176c285c9d280d33d36f44eeb2b23ef5845bedd0
= <=>
2025-03-09 12:27:01 -0300

    create car on the screen

--- car_manager.py
+++ car_manager.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
@@ -5,7 +6,15 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
-        super().__init__()
-        self.shape("square")
+        self.all_cars = []
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid=2, stretch_len=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint(-250, 250)
+        new_car.goto(x=300, y=random_y)
+        self.all_cars.append(new_car)
